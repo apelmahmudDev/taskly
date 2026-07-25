e75427ad91a7ca57dffa545e46a9178739c96ed2
Apel Mahmud <apelmahmud.dev@gmail.com>
2026-07-25 21:52:01 +0600

    feat: remove unused image assets and components to clean up the codebase

--- hooks/use-theme-color.ts
+++
@@ -1,8 +0,0 @@
-import { Colors } from '@/constants/theme';
-
-export function useThemeColor(
-  color: string | undefined,
-  colorName: keyof typeof Colors
-) {
-  return color ?? Colors[colorName];
-}
